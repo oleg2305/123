b92f8271f8c7de1fe92c0cec14f2074e304c2e29
Oleg <oleg@kozlov.msk.ru>
2017-10-20 11:00:59 +0000

    debian+phpmyadmin

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -x
+apt-get install apache2 php phpmyadmin mariadb-server php-imap mcrypt
+/usr/bin/mysql_secure_installation
+mysql -uroot -p -e "CREATE USER 'myadmin'@'localhost' IDENTIFIED BY '230568'; GRANT ALL PRIVILEGES ON *.* TO 'myadmin'@'localhost' WITH GRANT OPTION;"
