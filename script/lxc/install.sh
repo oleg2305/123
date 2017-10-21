35428320ae879efd20e1fc2f207c488e8cb55ec3
Oleg <oleg@kozlov.msk.ru>
2017-10-21 13:18:00 +0300

    lxc+bridge

---
+++ script/lxc/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get -y install lxc libvirt0 libpam-cgroup libpam-cgfs bridge-utils ferm
+brctl addbr br0
+echo "
+allow-hotplug br0
+iface br0 inet static
+	bridge_stp off 
+        address 192.168.123.1
+	broadcast 192.168.123.255
+	netmask 255.255.255.0
+	    " >> /etc/network/interfaces
+ifup br0
