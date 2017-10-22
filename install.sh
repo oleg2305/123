16cb27bc46756846668b22b22ab4b50f62fd6c2b
Oleg <oleg@kozlov.msk.ru>
2017-10-22 16:59:29 +0300

    1

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 if (whiptail --title "Service installation script" --yesno "Do you want to configure the host - lxc, fail2ban, ferm?" 8 78) then
-	$(pwd)/script/lxc/install.sh
+	if [ "$(id -u)" != "0" ]; then
+		whiptail --title "Error" --msgbox "This script must be run as sudo" 8 78
+		exit 1
+	else
+		$(pwd)/script/lxc/install.sh
+	fi
 else
 	echo "User selected No, exit status was $?."
 fi
